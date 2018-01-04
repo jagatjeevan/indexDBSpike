5133e910e6525349d5cd60fd12fef6dab510af8f
Jagat Jeevan Sahoo <jagatsah@thoughtworks.com>
2018-01-04 18:52:02 +0530

    adding config for the indexDB database

--- indexedDB.js
+++ indexedDB.js
@@ -1,39 +1,59 @@
 console.group("Code is written in ES6.");
-console.log("This code would work in Chrome latest version. Incase you get any error, please try converting es6 to es5 and then run the code.");
-console.log("Use this url to do so: https://babeljs.io/repl/");
+  console.log("This code would work in Chrome latest version. Incase you get any error, please try converting es6 to es5 and then run the code.");
+  console.log("Use this url to do so: https://babeljs.io/repl/");
 console.groupEnd();
 
 let openRequest;
 let databaseName = 'indexedDBSpike';
 
+let dbConfig = {
+  name: databaseName,
+  version: 1,
+  storeNames: {
+    pipelines: 'pipelines'
+  },
+  instance: {},
+  defaultErrorHandling: (err) => {
+    logStatement.updateLogger("Error in ", err);
+  },
+  setDefaultErrorHandling: (request) => {
+    if("onerror" in request) 
+      request.onerror = db.defaultErrorHandling;
+
+    if("onblocked" in request) 
+      request.onblocked = db.defaultErrorHandling;
+  }
+}
+
 document.querySelector('#createDB').addEventListener('click', () => {
-  openRequest = window.indexedDB.open(databaseName); // we passed the name of the Database.
+  openRequest = indexedDB.open(dbConfig.name, dbConfig.version); // we passed the name of the Database.
 
   // If indexedDBSpike database is not present, or the version is old, this method would get called.
   openRequest.onupgradeneeded = (e) => {
     let db = openRequest.result;
-    let store = db.createObjectStore("pipelines", { keyPath: "pipelineName"});
+    let store = db.createObjectStore(dbConfig.storeNames.pipelines, { keyPath: "pipelineName", autoIncrement: true });
+
     let authorName = store.createIndex('by_author', 'author');
     let pipelineName = store.createIndex('by_pipeline', 'pipelineName', { unique: true});
 
-    logStatement.updateLogger("Database created");
-    
+    logStatement.updateLogger("Database creating");
+
     // Populate initial data
     store.put({pipelineName: "PromoFrontend", author: "Jagat"});
     store.put({pipelineName: "PromoBackend", author: "Raghavendra"});
     store.put({pipelineName: "PromoSQL", author: "Rajshekhar"});
+
+    logStatement.updateLogger("Database created");
   }
 
-  openRequest.success = (e) => {
-    logStatement.updateLogger("Database created successfully");
-    console.log("Database opened");
-    db = openRequest.result;
+  openRequest.success = () => {
+    dbConfig.instance = openRequest.result;
+    logStatement.updateLogger("Database opened");
   }
 
   // Incase of failure at any point regarding indexedDB, this method would get called.
-  openRequest.onerror = (error) => {
-    logStatement.updateLogger("Error occured ", error);
-  }
+  // Since we are handling error on generic / common below code is the same as openRequest.onerror.
+  dbConfig.setDefaultErrorHandling(openRequest);
 
 });
 
@@ -46,14 +66,11 @@ document.querySelector('#deleteDB').addEventListener('click', () => {
     db.close();
     var deleteRequest = indexedDB.deleteDatabase(databaseName);
 
-    deleteRequest.onsuccess = () => {
-      logStatement.updateLogger("Database deleted successfully");
-    }
+    deleteRequest.onsuccess = logStatement.updateLogger("Database deleted successfully");
 
-    deleteRequest.onblocked = (err) => {
-      logStatement.updateLogger("Database could not be deleted. See console for error");
-      console.log("Database could not be deleted.", err);
-    }
+    // Incase of failure at any point regarding indexedDB, this method would get called.
+    // Since we are handling error on generic / common below code is the same as openRequest.onerror.
+    dbConfig.setDefaultErrorHandling(openRequest);
   } else {
     logStatement.updateLogger("database not present");
   }
